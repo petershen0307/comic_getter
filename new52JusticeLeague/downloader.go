819d3b00dc0cf17db82314ad73c4073dc67d23af
PeterShen <petershen0307@hotmail.com>
2017-03-26 23:48:28 +0800

    download comic by golang

---
+++ new52JusticeLeague/downloader.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+const urlTemplate = "http://www.readcomics.tv/images/manga/justice-league/%02d/%d.jpg"
+
+type errorCode int
+
+const (
+	eSuccess = errorCode(iota)
+	ePageNotFound
+	eFileExisted
+)
+
+func downloadOneChapter(ch int) {
+	for i := 1; downloadOnePage(ch, i) != ePageNotFound; i++ {
+	}
+}
+
+func downloadOnePage(ch, page int) errorCode {
+	dir := fmt.Sprintf("%02d", ch)
+	fileName := fmt.Sprintf("%d.jpg", page)
+	if isFileExist(dir, fileName) {
+		return eFileExisted
+	}
+	timeoutRequest := http.Client{Timeout: time.Second * 10}
+	url := fmt.Sprintf(urlTemplate, ch, page)
+	response, err := timeoutRequest.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	if response.StatusCode == 404 {
+		fmt.Println("Page not found! url: ", url)
+		return ePageNotFound
+	}
+	fmt.Println("Content Length: ", response.ContentLength)
+
+	writeFile(response.Body, dir, fileName)
+	return eSuccess
+}
+
+func isFileExist(dir, fileName string) bool {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		fmt.Println("Create dir: ", dir)
+		if err := os.Mkdir(dir, os.ModeDir); err != nil {
+			panic(err)
+		}
+	}
+	// check path exist or not
+	// if path exist os.Stat() return err will be nil, can't use os.IsExist()
+	/*
+		os.IsExist(), os.IsNotExist() reference: https://gist.github.com/mastef/05f46d3ab2f5ed6a6787
+		os.IsExist(err) is good for cases when you expect the file to not exist yet,
+		but the file actually exists :
+	*/
+	path := fmt.Sprintf("%s/%s", dir, fileName)
+	if _, err := os.Stat(path); err == nil {
+		fmt.Println("File exist: ", path)
+		return true
+	}
+	return false
+}
+
+func writeFile(reader io.Reader, dir, fileName string) {
+	if isFileExist(dir, fileName) {
+		return
+	}
+	data, err := ioutil.ReadAll(reader)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("data length: ", len(data))
+	path := fmt.Sprintf("%s/%s", dir, fileName)
+	fo, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		panic(err)
+	}
+	defer fo.Close()
+	fo.Write(data)
+}
+
+func main() {
+	downloadOneChapter(0)
+}
