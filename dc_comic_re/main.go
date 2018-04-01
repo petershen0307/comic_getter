549ce7c68c0ff1377a2dd088b5ae4d745c9254a0
PeterShen <petershen0307@hotmail.com>
2018-04-01 20:48:39 +0800

    update testing code

--- dc_comic_re/main.go
+++ dc_comic_re/main.go
@@ -12,12 +12,13 @@ func main() {
 	logClient.Log(logClient.LevelDebug, "hello")
 	//testDownload()
 	testExtractor()
+	//testExtractAllImages()
 }
 
 func testDownload() {
-	buf, bRet := download("http://readcomicbooksonline.org/the-hellblazer-2016")
+	buf, bRet := download("http://readcomicbooksonline.org/reader/The_Hellblazer_2016/The_Hellblazer_2016_Issue_01/?q=fullchapter")
 	if bRet && buf != nil {
-		fo, err := os.OpenFile("test/testPage.html", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0755)
+		fo, err := os.OpenFile("test/all_pic.html", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0755)
 		if err != nil {
 			logClient.Log(logClient.LevelDebug, "open file failed")
 		}
@@ -36,6 +37,20 @@ func testExtractor() {
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(fo)
 	chapterList := extractChapter(buf.String())
+	if chapterList == nil {
+		logClient.Log(logClient.LevelDebug, "chapterList is nil")
+	}
+	logClient.Log(logClient.LevelDebug, "chapterList:%+v, len:%v", chapterList, len(chapterList))
+}
+
+func testExtractAllImages() {
+	fo, err := os.OpenFile("C:/Users/PC/Desktop/code/go/test/all_pic.html", os.O_RDONLY, 0755)
+	if err != nil {
+		logClient.Log(logClient.LevelDebug, "open file failed")
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(fo)
+	chapterList := extractChapterImages(buf.String())
 	if chapterList == nil {
 		logClient.Log(logClient.LevelDebug, "chapterList is nil")
 	}
